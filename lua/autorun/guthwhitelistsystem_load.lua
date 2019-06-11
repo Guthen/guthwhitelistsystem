61c0c854d145b457eb2d12781d1dd387e3cbb31f
Guthen <33220603+Guthen@users.noreply.github.com>
2019-06-11 19:00:55 +0200

    Create lua/autorun/guthwhitelistsystem_load.lua

---
+++ lua/autorun/guthwhitelistsystem_load.lua
@@ -0,0 +1,5 @@
+guthwhitelistsystem = {}
+guthlevelsystem.Author = "Guthen"
+guthwhitelistsystem.Version = "0.1.0"
+guthwhitelistsystem.Link = "https://github.com/Guthen/guthwhitelistsystem"
+guthwhitelistsystem.Discord = "https://discord.gg/eKgkpCf"
