5a8d78e27e8c778ce3da525ca7ba4ae32585291c
Guthen <33220603+Guthen@users.noreply.github.com>
2019-06-11 19:07:22 +0200

    Update guthwhitelistsystem_load.lua

--- lua/autorun/guthwhitelistsystem_load.lua
+++ lua/autorun/guthwhitelistsystem_load.lua
@@ -1,5 +1,13 @@
 guthwhitelistsystem = {}
-guthlevelsystem.Author = "Guthen"
+guthwhitelistsystem.Author = "Guthen"
 guthwhitelistsystem.Version = "0.1.0"
 guthwhitelistsystem.Link = "https://github.com/Guthen/guthwhitelistsystem"
 guthwhitelistsystem.Discord = "https://discord.gg/eKgkpCf"
+
+if SERVER then
+    include( "guthwhitelistsystem/sv_base.lua" )
+else
+    include( "guthwhitelistsystem/cl_base.lua" )
+end
+
+print( ("[guthwhitelistsystem] - Made by %s in version %s, type 'guthwhitelistsystem_info' for more info."):format( guthwhitelistsystem.Author, guthwhitelistsystem.Version ) )
