a3781e8ead69f62680ced3da8a81a6f99b4398b0
Guthen <33220603+Guthen@users.noreply.github.com>
2020-09-19 08:22:28 +0200

    🆕 Patch : v1.5.2

--- lua/autorun/guthwhitelistsystem_load.lua
+++ lua/autorun/guthwhitelistsystem_load.lua
@@ -1,6 +1,6 @@
 guthwhitelistsystem = guthwhitelistsystem or {}
 guthwhitelistsystem.Author  = "Guthen"
-guthwhitelistsystem.Version = "1.5.1"
+guthwhitelistsystem.Version = "1.5.2"
 guthwhitelistsystem.Link    = "https://github.com/Guthen/guthwhitelistsystem"
 guthwhitelistsystem.Discord = "https://discord.gg/eKgkpCf"
 
